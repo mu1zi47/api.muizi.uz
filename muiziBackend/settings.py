b468cd9b4b32ba337a8c1ff508edd8186cf38e1d
Муиз Валиев <valiyevmuiz0407@gmail.com>
2025-07-17 21:10:39 +0500

    fix

--- muiziBackend/settings.py
+++ muiziBackend/settings.py
@@ -4,13 +4,13 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-# DEBUG = os.environ.get("DEBUG", "False") == "True"
-# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
-SECRET_KEY = 'django-insecure-6cer=l&qlbzukd#+je*i%d)u0)r6t8e($^j9xc4(0xjl(4m(ko'
-DEBUG=True
-ALLOWED_HOSTS = []
+# SECRET_KEY = 'django-insecure-6cer=l&qlbzukd#+je*i%d)u0)r6t8e($^j9xc4(0xjl(4m(ko'
+# DEBUG=True
+# ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [

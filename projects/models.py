8f83f6e539f3ff21569626437688fd81a30ce33e
Муиз Валиев <valiyevmuiz0407@gmail.com>
2025-08-06 15:12:40 +0500

    fix

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 class Technology(models.Model):
     name = models.CharField('Технология', max_length=100, unique=True)
@@ -12,13 +13,10 @@ class Projects(models.Model):
     link = models.URLField('Ссылка')
     description = models.CharField('Описание', max_length=255, blank=True, default="")
     image = models.ImageField('Фотография', upload_to='projects/')
-    
+    year = models.DateField('Дата', null=True, blank=True)
+
     # здесь связь многие-ко-многим
-    technologies = models.ManyToManyField(
-        Technology,
-        related_name='projects',
-        blank=True
-    )
+    technologies = models.ManyToManyField(Technology, related_name='projects', blank=True)
 
     def __str__(self):
         return self.name
